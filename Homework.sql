3c98072f1707ae4410d20ff02713a9e45ebdad71
Alex108108 <alex108108@mail.ru>
2022-12-14 16:18:05 +0300

    update homework SQL-queries

--- Homework.sql
+++ Homework.sql
@@ -13,30 +13,30 @@ INSERT INTO otusQA VALUES(4,'Around the Horn', 'Thomas Hardy', '120 Hanover Sq.'
 INSERT INTO otusQA VALUES(5,'Berglunds snabbkop', 'Christina Berglund', 'Berguvsvagen 8', 'Lulea', 'S-958 22', 'Sweden');
 
 1
-Выведите значение колонки ContactName
+Show list column with value ContactName
 SELECT ContactName 
 from otusQA
 2
-Выведите все возможные значения колонки Country
+Show list all possible column with values Country
 SELECT DISTINCT Country
 from otusQA
 3
-Выведите все записи, где City имеет значение London
+Show list all records where City is London
 SELECT *
 from otusQA
 WHERE City = 'London'
 4
-Выведите все записи, где City не равен London
+Show list all records where City is not equal to London
 SELECT *
 from otusQA
 WHERE City != 'London'
 5
-Выберите все записи, где Country равна Mexico и Postal Code 05021
+Select all records where Country is Mexico and Postal Code 05021
 SELECT *
 from otusQA
 WHERE country = 'Mexico' AND postalcode = '05021'
 6
-Выберете все записи у которых Country равна Mexico или Sweden
+Select all records with Country equal to Mexico or Sweden
 SELECT *
 from otusQA
 WHERE country = 'Mexico' OR country = 'Sweden'
